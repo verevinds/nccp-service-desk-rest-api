64e5c66b5e022ea7aec40484cd8e75d17aab7467
verevinds <verevinds@yandex.ru>
2020-04-23 13:53:32 +0700

    Fix typo on the commentaries

--- app/models/category.model.js
+++ app/models/category.model.js
@@ -1,37 +1,32 @@
 /**
- *  Модель отвечающая за структуру таблици "Categorys"
- *  "Категория" - это первый параметр выбираемый в инциденте.
+ *  Модель таблицы "Categories"
+ *  Категория - это первый параметр выбираемый в инциденте.
  * Он отвечает за то к какому отделу отправиться инцидент.
  *
  *  Связи:
  *    One-To-Many:
- *      "Propertys-Categorys" по значению "propertyId"
- *      "Options-Categorys" по значению "optionId"
- *      "Categorys-Responsibles" по значению "categoryId"
+ *      "Properties-Categories" по значению "Id"
+ *      "Options-Categories" по значению "Id"
+ *      "Categories-Responsibles" по значению "categoryId"
  *
  *****************************************************************
  *
- *  Model responsible for the structure of the "Categorys" table
+ *  Model responsible for the structure of the "Categories" table
  *  "Category" is the first parameter selected in the incident.
  * He is responsible for which department the incident will go to.
  *
  *  Associations:
  *    One-To-Many:
- *      Propertys-Categorys by value "propertyId"
- *      Options-Categorys by value "optionId"
- *      Categorys-Responsibles by value "categoryId"
+ *      Properties-Categories by value "Id"
+ *      Options-Categories by value "Id"
+ *      Categories-Responsibles by value "categoryId"
  */
 
 module.exports = (sequelize, Sequelize) => {
-  const category = sequelize.define('category', {
+  const Category = sequelize.define('category', {
     name: {
       type: Sequelize.STRING,
     },
-    optionId: {
-      type: Sequelize.INTEGER,
-    },
-    propertyId: {
-      type: Sequelize.INTEGER,
-    },
   });
+  return Category;
 };
