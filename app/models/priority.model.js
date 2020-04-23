56309035b275faf1a1604f8a10c069eda870ac82
verevinds <verevinds@yandex.ru>
2020-04-23 10:11:39 +0700

    Create model sequelize form table priority

---
+++ app/models/priority.model.js
@@ -0,0 +1,15 @@
+modules.exports = (sequelize, Sequelize) => {
+  const Priority = sequelize.define('priority', {
+    level: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: false,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+  });
+
+  return Priority;
+};
