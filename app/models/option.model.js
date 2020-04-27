89593c3789207b6f68c01ae8a1dcb358d67d5414
verevinds <verevinds@yandex.ru>
2020-04-27 13:19:05 +0700

    Create "Option" model

---
+++ app/models/option.model.js
@@ -0,0 +1,31 @@
+//
+//   ! Модель таблицы "Option"
+//    Опция - это третий параметр выбираемый в инциденте.
+//   Он отвечает за группировку инцидентов по поппулятным запросам.
+//
+//    Связи:
+//      One-To-Many:
+//        "Categories-Options" по значению "categoryId"
+//
+//    Model table "Property"
+//    Option is the third parameter selected in the incident.
+//  He is responsible for grouping incidents according to popular requests.
+//
+//    Associations:
+//      One-To-Many:
+//        "Categories-Options" by the value of "categoryId"
+//
+
+module.exports = (sequelize, Sequelize) => {
+  const Option = sequelize.define('option', {
+    name: {
+      type: Sequelize.STRING,
+    },
+    categoryId: {
+      type: Sequelize.INTEGER,
+      field: 'category_id',
+    },
+  });
+
+  return Option;
+};
