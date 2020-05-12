da615f372c2146e55b22de65f1b541177d37dee7
verevinds <verevinds@yandex.ru>
2020-05-12 14:54:07 +0700

    Change model incident

--- app/models/incident.model.js
+++ app/models/incident.model.js
@@ -4,10 +4,6 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.DATE,
       field: 'start_work',
     },
-    dateCreate: {
-      type: Sequelize.DATE,
-      field: 'date_create',
-    },
     currentResponsible: {
       type: Sequelize.INTEGER,
       field: 'current_responsible',
@@ -26,23 +22,8 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.INTEGER,
       field: 'department_id',
     },
-    positionId: {
-      type: Sequelize.INTEGER,
-      field: 'position_id',
-    },
-    name: {
-      type: Sequelize.STRING,
-    },
-    email: {
-      type: Sequelize.STRING,
-    },
-    number: {
-      type: Sequelize.STRING,
-    },
-    phone1: {
-      type: Sequelize.STRING,
-    },
-    phone2: {
+    numberInitiator: {
+      field: 'number_initiator',
       type: Sequelize.STRING,
     },
     categoryId: {
