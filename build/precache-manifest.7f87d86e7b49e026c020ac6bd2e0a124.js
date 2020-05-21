8cdfd15136715c03c17272a66fdbd46354c039f1
verevinds <verevinds@yandex.ru>
2020-05-21 14:15:44 +0700

    Update build

---
+++ build/precache-manifest.7f87d86e7b49e026c020ac6bd2e0a124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77afff93e3618a0ce49c315bbe53d8f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "309b02d9b3747c84f810",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2f13a627ffd791282b72",
+    "url": "/static/css/main.865a11c6.chunk.css"
+  },
+  {
+    "revision": "309b02d9b3747c84f810",
+    "url": "/static/js/2.39842ab2.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/static/js/2.39842ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f13a627ffd791282b72",
+    "url": "/static/js/main.773c9f90.chunk.js"
+  },
+  {
+    "revision": "5a536e7e8d2f1c041a41",
+    "url": "/static/js/runtime-main.3fa681df.js"
+  }
+]);
